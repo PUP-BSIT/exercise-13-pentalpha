c2806006ceae66040b6b6cb0cca1ff2ff082a9a0
espinola_frankiejosh <joshmespinola@gmail.com>
2025-06-02 14:43:01 +0800

    add espinola module (#4)
    * add espinola module
    
    * update espinola module

---
+++ pentalpha/espinola.py
@@ -0,0 +1,28 @@
+from os import system
+
+def espinola_profile():
+
+    while True:
+        system('cls')
+        print("Hello! I'm Frankie Josh M. Espinola!\n")
+        print("1. Hobbies")
+        print("2. Goals")
+        print("3. Motto")
+        print("0. Exit\n")
+
+        menu_choice = int(input("Enter your choice: "))
+
+        match (menu_choice):
+            case 1: 
+                print("My hobbies are: Riding Motorcyle, Playing Online Games")
+            case 2:
+                print("My goals is to graduate and help the family.")
+            case 3:
+                print("My motto in life is:")
+                print("Hard is part of life. Pick yours.")
+            case 0:
+                break
+            
+        input()
+
+espinola_profile()
